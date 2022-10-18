4f17b419233f8cb4e32e309236b4586fe3e60dd2
carlos <carl.devfs@gmail.com>
2022-10-18 15:56:48 +0200

    complete photographer medias style

--- src/factories/media.ts
+++ src/factories/media.ts
@@ -1,7 +1,7 @@
 import { Media } from "../types";
 
 export function MediaFactory(data: Media) {
-  const { photographerId, title, image, video } = data;
+  const { photographerId, title, image, video, likes } = data;
 
   const src = `assets/photographers/${photographerId}/${image ? image : video}`;
 
@@ -23,14 +23,44 @@ export function MediaFactory(data: Media) {
     return media;
   };
 
+  const getBottombanner = () => {
+    const bannerDiv = document.createElement("div");
+    bannerDiv.classList.add("media-banner");
+
+    const titleP = document.createElement("p");
+    titleP.textContent = title;
+
+    const likesContainer = document.createElement("div");
+    likesContainer.classList.add("media-banner-likes");
+    const likesNumber = document.createElement("p");
+    likesNumber.textContent = likes.toString();
+    likesNumber.classList.add("likes-number");
+
+    const img = document.createElement("img");
+    img.setAttribute("alt", "likes");
+    img.setAttribute("src", "../assets/icons/heart.svg");
+    img.classList.add("likes-img");
+
+    likesContainer.appendChild(likesNumber);
+    likesContainer.appendChild(img);
+    bannerDiv.appendChild(titleP);
+    bannerDiv.appendChild(likesContainer);
+    return bannerDiv;
+  };
+
   function getMediaDOM() {
     const article = document.createElement("article");
     const media = getImg();
 
     const link = document.createElement("a");
     link.setAttribute("href", src);
+    link.setAttribute("aria-label", `Ouvre l'image ${title}`);
     link.appendChild(media);
+
+    const bottomBanner = getBottombanner();
+
     article.appendChild(link);
+    article.appendChild(bottomBanner);
     return article;
   }
 
