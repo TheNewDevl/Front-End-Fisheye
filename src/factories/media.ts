be1aacf1ccd6645358be84a2e178ef88b3493e9e
carlos <carl.devfs@gmail.com>
2022-10-25 23:00:18 +0200

    add: change likes icon and number color let user know if a media is liked or not

--- src/factories/media.ts
+++ src/factories/media.ts
@@ -1,6 +1,26 @@
 import { Media } from "../types.js";
 import { likesHelper } from "./likes.js";
 
+const likedColor = "#D3573C";
+const unlikedColor = "#911C1C";
+const heartSvg = (isLiked: boolean) => {
+  return `<svg role="img" aria-label="likes" width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M9.5 18.35L8.23125 17.03C3.725 12.36 0.75 9.28 0.75 5.5C0.75 2.42 2.8675 0 5.5625 0C7.085 0 8.54625 0.81 9.5 2.09C10.4538 0.81 11.915 0 13.4375 0C16.1325 0 18.25 2.42 18.25 5.5C18.25 9.28 15.275 12.36 10.7687 17.04L9.5 18.35Z"
+      fill="${isLiked ? likedColor : unlikedColor}"/>
+  </svg>`;
+};
+
+/** Will update the likes number and icon, depending on like param */
+const setLikesColor = (
+  target: HTMLButtonElement,
+  number,
+  like: "like" | "unlike"
+): void => {
+  const svgPath = target.querySelector("path");
+  svgPath.setAttribute("fill", like === "like" ? likedColor : unlikedColor);
+  number.style.color = like === "like" ? likedColor : unlikedColor;
+};
+
 export function MediaFactory(data: Media) {
   const { photographerId, title, image, video, likes, date, id } = data;
   const { savedLikes } = likesHelper(id, photographerId);
@@ -37,9 +57,11 @@ export function MediaFactory(data: Media) {
     if (isLiked) {
       removeLike();
       numberContainer.textContent = likes.toString();
+      setLikesColor(e.currentTarget, numberContainer, "unlike");
     } else {
       addLike();
       numberContainer.textContent = (likes + 1).toString();
+      setLikesColor(e.currentTarget, numberContainer, "like");
     }
 
     // when this event is caught, the total likes will be updated
@@ -54,27 +76,23 @@ export function MediaFactory(data: Media) {
     const titleP = document.createElement("p");
     titleP.textContent = title;
 
-    const likesContainer = document.createElement("button");
-    likesContainer.classList.add("media-banner-likes");
-    likesContainer.setAttribute("aria-label", "Likes");
+    const likesBtn = document.createElement("button");
+    likesBtn.classList.add("media-banner-likes");
+    likesBtn.setAttribute("aria-label", "Likes");
 
     const likesNumber = document.createElement("span");
     likesNumber.textContent = (isLiked ? likes + 1 : likes).toString();
     likesNumber.classList.add("likes-number");
+    likesNumber.style.color = isLiked ? likedColor : unlikedColor;
 
-    const img = document.createElement("img");
-    img.setAttribute("alt", "likes");
-    img.setAttribute("src", "../assets/icons/heart.svg");
-    img.classList.add("likes-img");
+    likesBtn.appendChild(likesNumber);
+    likesBtn.insertAdjacentHTML("beforeend", heartSvg(isLiked));
 
-    likesContainer.appendChild(likesNumber);
-    likesContainer.appendChild(img);
     bannerDiv.appendChild(titleP);
-    bannerDiv.appendChild(likesContainer);
+    bannerDiv.appendChild(likesBtn);
 
-    //LIKES MANAGEMENT
-    likesContainer.addEventListener("click", handleLike);
-    //////
+    //init click event handle likes
+    likesBtn.addEventListener("click", handleLike);
 
     return bannerDiv;
   };
